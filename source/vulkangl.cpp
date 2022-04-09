b8bcfa05b87525459cdbef2f7d60c397ca541854
Felix Kugler <relgukxilef@gmail.com>
2022-04-09 03:30:37 +0200

    Implement vkDestroySurfaceKHR

--- source/vulkangl.cpp
+++ source/vulkangl.cpp
@@ -1,2 +1,13 @@
-#include <vulkan.h>
+#include <vulkan/vulkan.h>
 
+#include <iostream>
+
+#include <GLES2/gl2.h>
+
+VKAPI_ATTR void VKAPI_CALL
+vkDestroySurfaceKHR(
+    VkInstance instance, VkSurfaceKHR surface,
+    const VkAllocationCallbacks *pAllocator
+) {
+    std::cout << "vkDestroySurfaceKHR" << std::endl;
+}
